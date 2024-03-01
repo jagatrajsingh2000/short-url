f57349d4b6a59324382e2cf6ae5b1acd0ea9cf92
jagatrajsingh2000 <jagatrajsingh2000@gmail.com>
2024-03-01 10:07:37 +0530

    Ui:created

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const express = require('express');
 const URL = require('./models/url');
 const urlRoutes = require('./routes/url');
-
+const staticRoutes = require('./routes/staticRouter');
+const path = require('path');
 const {mongoDbConnection} = require('./connection');
 const app = express();
 const PORT = 8000
-
-app.use(express.json());
 
 // database collection
 mongoDbConnection("mongodb://localhost:27017/url").then(()=> console.log("mongodb connected"))
 .catch(err => console.log("error connecting", err))
 
+app.set("view engine", "ejs");
+app.set('views', path.resolve("./view"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:false }));
+app.use("/static",staticRoutes);
+
+app.get("/test", async (req, res) => {
+    const allUrls = await URL.find({});
+    return res.render('home',{
+        urls: allUrls,
+    })
+})
+
+
 app.use('/url',urlRoutes);
-app.get("/:shortId",async (req,res)=>{
+app.get("/url/:shortId",async (req,res)=>{
     console.log("helloo");
     const shortId = req.params.shortId;
     const result = await URL.findOneAndUpdate(
