18512877a3960ee7910ad9817609b1ee07ff1169
jagatrajsingh2000 <jagatrajsingh2000@gmail.com>
2024-03-05 21:26:39 +0530

    jwt added

--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,25 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+// function setUser(id, user) {
+//   sessionIdToUserMap.set(id, user);
+// }
+
+// function getUser(id) {
+//   return sessionIdToUserMap.get(id);
+// }
+const jwt = require('jsonwebtoken');
+const secret = "Jagat123@"
+
+function setUser(user) {
+    return jwt.sign({
+        _id:user._id,
+        email: user.email,
+    }, secret);
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+    if(!token) return null;
+    return jwt.verify(token,secret);
 }
 
 module.exports = {
